b6672f90fb81a6826b1d8233b8b749ba690c0ec9
christian <75186171+samochristian2020@users.noreply.github.com>
2023-08-06 23:40:14 +0100

    Update query_1.sql

--- query_1.sql
+++ query_1.sql
@@ -1,1 +1,38 @@
+% -----------------------------------QUERY 1---------------------------------------------
+% ---------------------------------------------------------------------------------------
+SELECT Customer.CustomerId, 
+       Customer.FirstName, 
+       Customer.LastName, 
+       Artist.Name Artist_name, 
+       InvoiceLine.UnitPrice * SUM(InvoiceLine.Quantity)
+FROM Artist
+JOIN Album
+ON Artist.ArtistId = Album.ArtistId
+JOIN Track
+ON Track.AlbumId = Album.AlbumId
+JOIN InvoiceLine
+ON Track.TrackId=InvoiceLine.TrackId
+JOIN Invoice
+ON Invoice.InvoiceId =InvoiceLine.InvoiceId
+JOIN Customer
+ON Invoice.CustomerId = Customer.CustomerId
+WHERE Artist.Name IN(SELECT A_name 
+                     FROM (SELECT Artist.ArtistId, 
+			          Artist.Name A_name, 
+				  InvoiceLine.UnitPrice * SUM(InvoiceLine.Quantity) as earned
+                           FROM Artist
+                           JOIN Album
+                           ON Artist.ArtistId = Album.ArtistId
+                           JOIN Track
+                           ON Track.AlbumId = Album.AlbumId
+                           JOIN InvoiceLine
+                           ON Track.TrackId=InvoiceLine.TrackId
+                           JOIN Invoice
+                           ON Invoice.InvoiceId =InvoiceLine.InvoiceId
+                           GROUP BY 1,2
+                           ORDER BY 3 DESC
+                           LIMIT 1) sub1
+					  )
+GROUP BY 1
+ORDER BY 5 DESC
 
